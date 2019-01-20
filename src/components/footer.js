92c597fced31f25794b747af7f1c09e01c051796
HellMcnamara <dr.rubert@gmail.com>
2019-01-19 22:23:44 -0600

    second commit

---
+++ src/components/footer.js
@@ -0,0 +1,4 @@
+import React from "react"
+
+export default () => 
+<p>see you en space cowboy</p>
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div className="Wrapper">
+      <Header /> 
+      <h1>Hey I’m Jay,</h1>
+        <p>a Colombian designerd based <br></br>in Mexico City.</p>
+        <p>Product Designer at 23 Design.</p>
+        <div className="Social">
+            <a href="https://twitter.com/Noyonosoyesa" target="_blank">Twitter</a> -
+            <a href="https://dribbble.com/hellmcnamara" target="_blank">Dribbble</a> - 
+            <a href="https://www.instagram.com/noyonosoyesa/" target="_blank">Instagram</a> 
+        </div>
+    </div>
+  )
