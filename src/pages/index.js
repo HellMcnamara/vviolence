a49b26f5674b6b8c92fc84f4be9c3cff8fea8536
HellMcnamara <dr.rubert@gmail.com>
2019-01-19 23:14:10 -0600

    analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ export default () => (
         <p>a Colombian designerd based <br></br>in Mexico City.</p>
         <p>Product Designer at 23 Design.</p>
         <div className="Social">
-            <a href="https://twitter.com/Noyonosoyesa" target="_blank">Twitter</a> -
-            <a href="https://dribbble.com/hellmcnamara" target="_blank">Dribbble</a> - 
-            <a href="https://www.instagram.com/noyonosoyesa/" target="_blank">Instagram</a> 
+            <a href="https://twitter.com/Noyonosoyesa" target="_blank" rel="noopener">Twitter</a> -
+            <a href="https://dribbble.com/hellmcnamara" target="_blank" rel="noopener">Dribbble</a> - 
+            <a href="https://www.instagram.com/noyonosoyesa/" target="_blank" rel="noopener">Instagram</a> 
         </div>
     </div>
   )
