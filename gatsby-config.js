51f516e1fdbabee4148155012b5d91b7972b5173
HellMcnamara <dr.rubert@gmail.com>
2019-01-24 20:47:04 -0600

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,34 +2,15 @@
 module.exports = {
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-132838776-1",
+        // your google analytics tracking id
+        trackingId: `UA-132838776-1`,
         // Puts tracking script in the head instead of the body
-        head: true,
-        // Setting this parameter is optional
-        anonymize: false,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Any additional create only fields (optional)
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
-        },
-
-        resolve: `gatsby-plugin-hotjar`,
-        options: {
-        id: 1172575,
-        sv: 6,
-        },
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
     },
   ],
 }
