61b3150ef9b068fbc7f4ce35afefd15fd1f2e17f
HellMcnamara <dr.rubert@gmail.com>
2019-03-09 17:26:24 -0600

    Home Wt

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () => (
     <div className="Wrapper">
       <Header /> 
       <h1>Hey, I’m Jay!</h1>
-        <p>a Colombian designerd based in Mexico City.</p>
+        <p> a Colombian designerd based in Mexico City.</p>
         <div className="Social">
             <a href="https://twitter.com/Noyonosoyesa" target="_blank" rel="noopener noreferrer">Twitter</a> -
             <a href="https://dribbble.com/hellmcnamara" target="_blank" rel="noopener noreferrer">Dribbble</a> - 
